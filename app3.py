0a5051ae264a07de1e6ed0bad1dc2790030a883f
Sai Harshini <saiharshini2004@gmail.com>
2026-07-09 09:54:43 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+
+# Load model
+with open("trained_model.pkl","rb") as file:
+    model = pickle.load(file)
+
+# Load vectorizer
+with open("tfidf_vectorizer.pkl","rb") as file:
+    vectorizer = pickle.load(file)
+
+st.title("Customer Support Ticket Classification")
+
+st.write("Enter a customer support ticket below.")
+
+ticket = st.text_area("Ticket Description")
+
+if st.button("Predict"):
+
+    ticket = ticket.lower()
+
+    vector = vectorizer.transform([ticket])
+
+    prediction = model.predict(vector)
+
+    st.success("Predicted Ticket Type")
+
+    st.write(prediction[0])
