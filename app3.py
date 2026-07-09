412839f9a9102cbc6e024b68ee0a01b52d81c59b
Sai Harshini <saiharshini2004@gmail.com>
2026-07-09 09:21:17 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,35 +1,28 @@
-import gradio as gr
+import streamlit as st
 import pickle
 
 # Load model
-with open("trained_model.pkl", "rb") as file:
+with open("trained_model.pkl","rb") as file:
     model = pickle.load(file)
 
 # Load vectorizer
-with open("tfidf_vectorizer.pkl", "rb") as file:
+with open("tfidf_vectorizer.pkl","rb") as file:
     vectorizer = pickle.load(file)
 
+st.title("Customer Support Ticket Classification")
 
-def predict_ticket(text):
+st.write("Enter a customer support ticket below.")
 
-    text = text.lower()
+ticket = st.text_area("Ticket Description")
 
-    text_vector = vectorizer.transform([text])
+if st.button("Predict"):
 
-    prediction = model.predict(text_vector)
+    ticket = ticket.lower()
 
-    return prediction[0]
+    vector = vectorizer.transform([ticket])
 
+    prediction = model.predict(vector)
 
-demo = gr.Interface(
-    fn=predict_ticket,
-    inputs=gr.Textbox(
-        lines=5,
-        label="Enter Ticket Description"
-    ),
-    outputs=gr.Textbox(label="Predicted Ticket Type"),
-    title="Customer Support Ticket Classification",
-    description="Predict customer support ticket categories using Machine Learning."
-)
+    st.success("Predicted Ticket Type")
 
-demo.launch()
+    st.write(prediction[0])
